be44d1dc44ec57ae18a7f8b0b90620c6a180b4ff
KAVIYA P <103497554+kavee2729@users.noreply.github.com>
2023-03-17 13:47:02 +0530

    Add files via upload

---
+++ WATERCONS.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	        int x;
+	        scanf("%d",&x);
+	        if(x>=2000){
+	                printf("yes\n");
+	        }
+	        else{
+	                printf("no\n");
+	        }
+	}
+	return 0;
+}
+
